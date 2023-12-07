98067597876689af1184f25a6b146cd474cb7b3f
Ayomide24500 <Ayomideadisa83@gmail.com>
2023-12-07 14:12:49 +0100

    second

--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -1,11 +1,11 @@
 import mongoose, { connect } from "mongoose";
+import dotEnv from "dotenv";
+dotEnv.config();
 const URL: string | undefined = process.env.DATABASE_STRING;
 
-export const mainConnection = async () => {
+export const mainConnection = () => {
   try {
-    connect(
-      "mongodb+srv://Ayomide12:Ayo234@cluster0.31friv9.mongodb.net/Flowdb?retryWrites=true&w=majority"
-    ).then(() => {
+    connect("mongodb+srv://Ayomide12:Ayo234@cluster0.31friv9.mongodb.net/Flowdb?retryWrites=true&w=majority").then(() => {
       console.log("Database is now connected..🚀🚀🚀!");
     });
   } catch (error) {
